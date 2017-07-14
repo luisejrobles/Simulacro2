5233bd8c04a4a61c19524ef7696dc31edf54a7e7
luisejrobles <luisejimenezrobles@gmail.com>
2017-07-13 17:06:38 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Simulacro2.c
+ *
+ * Created: 13/07/2017 05:05:51 p. m.
+ * Author : LuisEduardo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
